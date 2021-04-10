3ec1ad236b19415222cd4578164b65a9a8b8b4df
Eric <lee.erictae@gmail.com>
2021-04-10 12:00:29 -0500

    subtract 1 if n is 2 or 3, translate it js

--- integer_break.js
+++ integer_break.js
@@ -1,3 +1,19 @@
 // Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
 
 // Return the maximum product you can get.
+
+
+function integerBreak(n) {
+  if (n == 1 | n == 3) {
+    return (n - 1);
+  }
+  var temp = 1;
+  while (n > 4) {
+    n = n - 3;
+    temp = temp * 3;
+  }
+  return (n * temp);
+
+}
+
+console.log(integerBreak(10));
--- integer_break.rb
+++ integer_break.rb
@@ -17,14 +17,22 @@
 # The maximum product can be obtained be repeatedly cutting parts of size 3 while size is greater than 4, keeping the last part as size of 2 or 3 or 4
 
 
+# if n is 2 or 3, subtract 1 from n
+
+
+
+
 def integer_break(n)
+  if n == 2 || n == 3
+    return n -1
+  end 
   temp = 1
   while n > 4
     n = n -3
-    temp = temp *3
+    temp = temp * 3
   end
-  return n * 3 
+  return n * temp
 end
 
-p integer_break(5)
+p integer_break(10)
 
